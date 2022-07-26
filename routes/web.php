8a4a8c263a0a8f7a9e8b5be41b25324e6b9146a1
Md. Bodiuzzaman Ripon <riponzamanbd@gmail.com>
2022-07-26 11:49:42 +0600

    init

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\RstController;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
-    return view('welcome');
+    return('This is the default route');
 });
 
 Route::get('/rst', [RstController::class, 'showData']);
